03d82b4b13b08862729ee5ee549e1ee5d3b71cc1
glockmane <glockmane@googlemail.com>
2024-09-18 21:20:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -eux pipefail
 # shellcheck source=/dev/null
 . /var/run/qubes/qubes-ns
 
-REMOTE="${REMOTE:-https://git.sr.ht/~qubes/proxy/blob/main}"
+REMOTE="${REMOTE:-https://github.com/glockmane/qubes-proxy/blob/main}"
 WORKDIR="/tmp"
 INET4_ADDR="$(echo "${NS1}" | sed 's/[0-9]*$/0/')"
 
