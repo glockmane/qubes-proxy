237584ab8d8a44f38ba52bee69d823623c18135f
glockmane <glockmane@googlemail.com>
2024-09-21 11:02:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ INET4_ADDR="$(echo "${NS1}" | sed 's/[0-9]*$/0/')"
 
 cd "${WORKDIR}"
 
-curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/restrict-firewall" -o restrict-firewall
+#curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/restrict-firewall" -o restrict-firewall
 curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/sing-box.service" -o sing-box.service
 curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/50_sing-box.conf" -o 50_sing-box.conf
 curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/config.json" -o config.json
@@ -23,11 +23,15 @@ sed -i "s#10.139.1.0#${INET4_ADDR}#" config.json
 
 sudo install -Dm644 -t /rw/bind-dirs/etc/sing-box "${WORKDIR}/config.json"
 sudo install -Dm644 -t /rw/bind-dirs/etc/systemd/system/ "${WORKDIR}/sing-box.service"
-sudo install -Dm755 -t /rw/config/qubes-firewall.d "${WORKDIR}/restrict-firewall"
+#sudo install -Dm755 -t /rw/config/qubes-firewall.d "${WORKDIR}/restrict-firewall"
 sudo install -Dm644 -t /rw/config/qubes-bind-dirs.d "${WORKDIR}/50_sing-box.conf"
 
 sudo cp /rw/config/rc.local /rw/config/rc.local.old
 echo 'systemctl --no-block restart sing-box.service' | sudo tee -a /rw/config/rc.local
+echo 'nft add rule ip qubes custom-input iifname singtun0 ip saddr 10.139.1.0/30 accept' | sudo tee -a /rw/config/rc.local
+echo 'nft add rule qubes custom-forward oifname eth0 counter drop' | sudo tee -a /rw/config/rc.local
+echo 'nft add rule ip6 qubes custom-forward oifname eth0 counter drop' | sudo tee -a /rw/config/rc.local
+echo 'nft add rule ip qubes custom-forward tcp flags syn / syn,rst tcp option maxseg size set rt mtu' | sudo tee -a /rw/config/rc.local
 
 url="$(curl -sSfL https://api.github.com/repos/SagerNet/sing-box/releases/latest | grep browser_download_url | grep linux-amd64.tar.gz | cut -d\" -f4)"
 curl --proto '=https' -tlsv1.2 -sSfL "${url}" | tar -zx --strip-components=1
